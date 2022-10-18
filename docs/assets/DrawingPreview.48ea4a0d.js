49d275393f7627ed59760410a736675ae9033a11
dengtuo <2284925818@qq.com>
2022-10-18 16:17:00 +0800

    feat: base2

---
+++ docs/assets/DrawingPreview.48ea4a0d.js
@@ -0,0 +1,1 @@
+import"./index.b2305bb9.js";import{ai as o}from"./index.b2305bb9.js";export{o as default};
--- docs/assets/DrawingPreview.d6dae9de.js
+++
@@ -1,1 +0,0 @@
-import"./index.19444ef7.js";import{ai as o}from"./index.19444ef7.js";export{o as default};
